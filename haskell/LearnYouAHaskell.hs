ba6bb0bdece9af9636a1c756be61459474e56598
Tyler Florez <qualiabyte@gmail.com>
2016-09-02 02:43:36 -0700

    Add replicate tests (Haskell)

--- haskell/LearnYouAHaskell.hs
+++ haskell/LearnYouAHaskell.hs
@@ -12,11 +12,25 @@ maximum' (x:xs)
     | otherwise = maxTail
     where maxTail = maximum' xs
 
-testChapter6 = do
-  putStrLn "Running Chapter 6 tests..."
+-- Recursive Replicate
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
+
+testMaximum = do
   let list = [ 1, 2, 3, 4, 5 ]
   let largest = maximum' list
   assert (largest == 5) "Recursive maximum should select largest from list"
+
+testReplicate = do
+  assert ((replicate 0 5) == []) "Replicate should repeat item zero times"
+  assert ((replicate 3 5) == [5, 5, 5]) "Replicate should repeat item three times"
+
+testChapter6 = do
+  putStrLn "Running Chapter 6 tests..."
+  testMaximum
+  testReplicate
   putStrLn "Finished Chapter 6 tests..."
 
 main = do
